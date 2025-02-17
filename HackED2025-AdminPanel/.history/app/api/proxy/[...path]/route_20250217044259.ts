cd28a9084922b7132308275288b573c98acffd3e
JaskaranSingh2 <jasonkaura@gmail.com>
2025-02-17 10:43:51 -0700

    com

--- HackED2025-AdminPanel/.history/app/api/proxy/[...path]/route_20250217044259.ts
+++
@@ -1,8 +0,0 @@
-// Add these headers to your API responses
-return NextResponse.json(data, {
-	headers: {
-		"Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
-		Pragma: "no-cache",
-		Expires: "0",
-	},
-});
